7075698453e72d2798d9075e898fb9aa5aafa37d
Namrata Acharya <namrataa@illinois.edu>
2022-05-20 11:43:05 -0500

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+    //const url = 'http://api.weatherstack.com/current?access_key=eb32c4869dad0c334d203892fbff1dde&query=37.8267,-122.4233&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=eb32c4869dad0c334d203892fbff1dde&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({url, json: true}, (error, {body}) => {
+
+        if (error) { //low level errors
+            callback('Unable to connect to weather service!', undefined)
+
+        } else if (body.error) {
+            callback('Unable to find location!', undefined)
+
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees. It feels like ' + body.current.feelslike + ' degrees.')
+
+        }
+
+    }) 
+}
+
+module.exports = forecast
---
+++ src/utils/geocode.js
@@ -0,0 +1,27 @@
+const { builtinModules } = require('module')
+const request = require('postman-request')
+
+const geocode = (address, callback) => {
+    //const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + 'Los%20Angeles.json?access_token=pk.eyJ1IjoibmFtcmF0YWFjaGFyeWEiLCJhIjoiY2wzYWR1cHN3MDBnMDNzczRkYjVpcXdkZyJ9.2jW_FvQTVX5w1hnZ5lZgmg&limit=1'
+    //const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoibmFtcmF0YWFjaGFyeWEiLCJhIjoiY2wzYWR1cHN3MDBnMDNzczRkYjVpcXdkZyJ9.2jW_FvQTVX5w1hnZ5lZgmg&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibmFtcmF0YWFjaGFyeWEiLCJhIjoiY2wzYWR1cHN3MDBnMDNzczRkYjVpcXdkZyJ9.2jW_FvQTVX5w1hnZ5lZgmg&limit=1'
+
+    request ({ url, json: true}, (error, {body}) => {
+
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
